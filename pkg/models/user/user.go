63ed2606e31f1ecec07869a0bb7ec97839eb3093
Brad Clawsie <brad@b7j0c.org>
2021-06-06 16:42:51 -0500

    added user test

--- pkg/models/user/user.go
+++ pkg/models/user/user.go
@@ -2,7 +2,15 @@
 package user
 
 import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
 	"github.com/grokloc/grokloc-go/pkg/models"
+	"github.com/grokloc/grokloc-go/pkg/schemas"
+	"github.com/grokloc/grokloc-go/pkg/security"
 )
 
 // exported user symbols
@@ -21,4 +29,73 @@ type Instance struct {
 	EmailDigest       string `json:"email_digest"`
 	Org               string `json:"org"`
 	Password          string `json:"password"`
+}
+
+// New creates a new user that hasn't been created before
+func New(displayName, email, org, password string) (*Instance, error) {
+	for _, v := range []string{displayName, email, org, password} {
+		if !security.SafeStr(v) {
+			return nil, errors.New("malformed user arg")
+		}
+	}
+	u := &Instance{Org: org, Password: password}
+	u.ID = uuid.NewString()
+	u.Meta.SchemaVersion = SchemaVersion
+	u.Meta.Status = models.StatusUnconfirmed
+
+	u.APISecret = uuid.NewString()
+	u.APISecretDigest = security.EncodedSHA256(u.APISecret)
+	u.DisplayName = displayName
+	u.DisplayNameDigest = security.EncodedSHA256(u.DisplayName)
+	u.Email = email
+	u.EmailDigest = security.EncodedSHA256(u.Email)
+
+	return u, nil
+}
+
+// Insert a new row.
+// TODO - make sure org exists and is active
+func (u *Instance) Insert(ctx context.Context, db *sql.DB, key []byte) error {
+	encryptedAPISecret, err := security.Encrypt(u.APISecret, key)
+	if err != nil {
+		return err
+	}
+	encryptedDisplayName, err := security.Encrypt(u.DisplayName, key)
+	if err != nil {
+		return err
+	}
+	encryptedEmail, err := security.Encrypt(u.Email, key)
+	if err != nil {
+		return err
+	}
+	q := fmt.Sprintf("insert into %s (id,api_secret,api_secret_digest,display_name,display_name_digest,email,email_digest,org,password,status,schema_version) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)",
+		schemas.UsersTableName)
+	result, err := db.ExecContext(ctx,
+		q,
+		u.ID,
+		encryptedAPISecret,
+		u.APISecretDigest,
+		encryptedDisplayName,
+		u.DisplayNameDigest,
+		encryptedEmail,
+		u.EmailDigest,
+		u.Org,
+		u.Password,
+		u.Meta.Status,
+		SchemaVersion)
+	if err != nil {
+		if models.UniqueConstraint(err) {
+			return models.ErrConflict
+		}
+		return err
+	}
+	inserted, err := result.RowsAffected()
+	if err != nil {
+		// the db does not support a basic feature
+		panic("cannot exec RowsAffected:" + err.Error())
+	}
+	if inserted != 1 {
+		return models.ErrRowsAffected
+	}
+	return nil
 }
---
+++ pkg/models/user/user_test.go
@@ -0,0 +1,95 @@
+// Package user models an user
+package user
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/grokloc/grokloc-go/pkg/env"
+	"github.com/grokloc/grokloc-go/pkg/state"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type UserSuite struct {
+	suite.Suite
+	ST *state.Instance
+}
+
+func (suite *UserSuite) SetupTest() {
+	var err error
+	suite.ST, err = state.NewInstanceFromLevel(env.Unit)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (suite *UserSuite) TestInsertUser() {
+	u, err := New(uuid.NewString(), uuid.NewString(), uuid.NewString(), uuid.NewString())
+	require.Nil(suite.T(), err)
+	err = u.Insert(context.Background(), suite.ST.Master, suite.ST.Key)
+	require.Nil(suite.T(), err)
+
+	// // duplicate
+	// err = u.Insert(context.Background(), suite.ST.Master)
+	// require.Error(suite.T(), err)
+	// require.Equal(suite.T(), models.ErrConflict, err)
+}
+
+func (suite *UserSuite) TestReadUser() {
+	// not found
+	// _, err := Read(context.Background(), suite.ST.RandomReplica(), uuid.NewString())
+	// require.Error(suite.T(), err)
+	// require.Equal(suite.T(), sql.ErrNoRows, err)
+
+	// o, err := New(uuid.NewString())
+	// require.Nil(suite.T(), err)
+	// err = u.Insert(context.Background(), suite.ST.Master)
+	// require.Nil(suite.T(), err)
+
+	// oRead, err := Read(context.Background(), suite.ST.RandomReplica(), o.ID)
+	// require.Nil(suite.T(), err)
+	// require.Equal(suite.T(), o.ID, oRead.ID)
+	// require.Equal(suite.T(), o.Name, oRead.Name)
+	// require.Equal(suite.T(), o.Owner, oRead.Owner)
+	// require.NotEqual(suite.T(), o.Meta.Ctime, oRead.Meta.Ctime)
+	// require.NotEqual(suite.T(), o.Meta.Mtime, oRead.Meta.Mtime)
+}
+
+func (suite *UserSuite) TestUpdateUserStatus() {
+	// o, err := New(uuid.NewString())
+	// require.Nil(suite.T(), err)
+
+	// // not yet inserted
+	// err = u.UpdateStatus(context.Background(), suite.ST.Master, models.StatusActive)
+	// require.Error(suite.T(), err)
+	// require.Equal(suite.T(), sql.ErrNoRows, err)
+
+	// // fix that
+	// err = u.Insert(context.Background(), suite.ST.Master)
+	// require.Nil(suite.T(), err)
+
+	// // demonstrate that the status is not active
+	// oRead, err := Read(context.Background(), suite.ST.RandomReplica(), o.ID)
+	// require.Nil(suite.T(), err)
+	// require.Equal(suite.T(), models.StatusUnconfirmed, oRead.Meta.Status)
+
+	// // update again
+	// err = u.UpdateStatus(context.Background(), suite.ST.Master, models.StatusActive)
+	// require.Nil(suite.T(), err)
+
+	// // re-read to be sure, check changed status
+	// oRead, err = Read(context.Background(), suite.ST.RandomReplica(), o.ID)
+	// require.Nil(suite.T(), err)
+	// require.Equal(suite.T(), models.StatusActive, oRead.Meta.Status)
+
+	// // None not allowed
+	// err = u.UpdateStatus(context.Background(), suite.ST.Master, models.StatusNone)
+	// require.Error(suite.T(), err)
+}
+
+func TestUserSuite(t *testing.T) {
+	suite.Run(t, new(UserSuite))
+}
