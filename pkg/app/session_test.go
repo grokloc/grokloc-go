a3fc0e9d6b376bf73785897463386ac2c253e02a
Brad Clawsie <brad@b7j0c.org>
2021-06-14 16:25:35 -0500

    add route for test

--- pkg/app/session_test.go
+++ pkg/app/session_test.go
@@ -3,9 +3,11 @@ package app
 import (
 	"context"
 	"log"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/grokloc/grokloc-go/pkg/env"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
@@ -24,8 +26,20 @@ func (s *SessionSuite) SetupTest() {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+	r := chi.NewRouter()
+	r.Route("/", func(r chi.Router) {
+		r.Use(s.srv.GetUserAndOrg)
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("content-type", "text/plain; charset=utf-8")
+			_, err := w.Write([]byte("OK"))
+			if err != nil {
+				panic(err.Error())
+			}
+		})
+	})
+
 	s.ctx = context.Background()
-	s.ts = httptest.NewServer(s.srv.Router())
+	s.ts = httptest.NewServer(r)
 }
 
 func (s *SessionSuite) TestFoundAndActive() {
